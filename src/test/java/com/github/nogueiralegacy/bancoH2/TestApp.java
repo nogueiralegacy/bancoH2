4c57bc0f2949ef1052da4235c618458bc2ea231c
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2023-02-10 09:33:25 -0300

    Codigo

---
+++ src/test/java/com/github/nogueiralegacy/bancoH2/TestApp.java
@@ -0,0 +1,13 @@
+package com.github.nogueiralegacy.bancoH2;
+
+/**
+ * Hello world!
+ *
+ */
+public class TestApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
